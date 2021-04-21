ed63f639cb31f78d211b3e37d5fe498d6154dd5a
WENGAN <970240739@qq.com>
2021-04-21 20:16:22 +0800

    commit java Hello file

---
+++ HELLO.java
@@ -0,0 +1,22 @@
+//java快速入门
+//public class Hello表示Hello是一个public公有的类
+//Hello{}表示一个类的开始和结束
+//public static void main(String[] args)表示一个主方法，即程序的入口
+//main() {}表示方法的开始和结束
+//System.out.println("hello,world~");表示输出"hello world~"到屏幕
+//;表示语句的结束
+public class Hello{
+
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println("wenagn is studying java!");
+	}
+}
+
+class Dog{
+
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println("Hello 狗");
+	}
+}
