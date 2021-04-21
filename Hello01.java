18e14d3f7684a398bc21065e5c2314b56a4b4933
WENGAN <970240739@qq.com>
2021-04-21 20:58:26 +0800

    commit 5 files

---
+++ Hello01.java
@@ -0,0 +1,33 @@
+/**
+ * @author wengan
+ * @version  1.1
+ */
+
+public class Hello01{
+
+	//编写一个main方法
+	public static void main(String[] args){
+        /*
+        转义字符训练
+         */
+		System.out.println("\t小明\t小红\t小芳\t大雄\n数学\t90\t67\t89\t93\n语文\t78\t90\t94\t92\n英语\t65\t83\t92\t84");
+
+	}
+}
+
+//在java中可以放多个类，但是public类只能有一个
+
+class Tiger{
+
+	public static void main(String[] args){
+		System.out.println("hello Tiger !");
+	}
+}
+
+class cat{
+
+	public static void main(String[] args){
+
+		System.out.println("hello cat!");
+	}
+}
