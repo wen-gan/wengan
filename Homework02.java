213f07b23a10474c92369bc7aa69932588a7ce27
WENGAN <970240739@qq.com>
2021-04-22 21:59:07 +0800

    commit 7 homework files

---
+++ Homework02.java
@@ -0,0 +1,9 @@
+//打印：姓名、性别、籍贯、住址到控制输出台上
+
+public class Homework02{
+
+	public static void main(String[] args){
+
+		System.out.println("姓名\t性别\t籍贯\t住址\n文淦\t男\t四川\t天津");
+	}
+}
---
+++ Homework03.java
@@ -0,0 +1,8 @@
+/*
+
+JDK、JRE和JVM的关系：
+1.JDK = JRE + 开发工具集（如：javac, java编译工具等）
+2.JRE = JVM + Java SE标准类库
+3.JDK = JVM + Java SE标准类库 + 开发工具集
+
+ */
