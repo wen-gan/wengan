10fb2a5ed1e3c30a89bf30e98369a1d1f5af616a
WENGAN <970240739@qq.com>
2021-04-27 14:45:26 +0800

    commit chapter3 4 files

---
+++ chapter3Homework03.java
@@ -0,0 +1,14 @@
+//保存两本书名，两个性别，两本书的价格
+public class chapter3Homework03{
+	public static void main(String[] args){
+		String name1 = "活着";
+		String name2 = "教父";
+		char gender1 = '男';
+		char gender2 = '女';
+		double price1 = 123.56;
+		double price2 = 100.11;
+		System.out.println(name1 + name2);
+		System.out.println(gender1 + gender2);//得到"男"字符的码值+"女"字符的码值
+		System.out.println(price1 + price2);
+	}
+}
---
+++ chapter3Homework04.java
@@ -0,0 +1,18 @@
+//作业4
+public class chapter3Homework04{
+	public static void main(String[] args){
+		String name = "姓名";
+		String age = "年龄";
+		String grade = "成绩";
+		String gender = "性别";
+		String habit = "爱好";
+		String name1 = "wengan";
+		int age1 = 22;
+		double grade1 = 100.0;
+		char gender1 = '男';
+		String habit1 = "basketball";
+		//一句话输出
+		System.out.println(name + "\t" + age + "\t" + grade + "\t" + gender + "\t" + habit + "\n" +
+		 name1 + "\t" + age1 + "\t" + grade1 + "\t" + gender1 + "\t" + habit1);
+	}
+}
